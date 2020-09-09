78cdc8de881bafa2da7029826118fe043c62cb41
Balint H <balint.haui@gmail.com>
2020-09-09 12:44:15 +0200

    feat: app-post component, app-add-comment component and api.service created, ng-bootstrap added

---
+++ frontEnd/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PostsComponent } from './components/posts/posts.component';
+import { AddCommentComponent } from './components/add-comment/add-comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent,
+    AddCommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
